20ca23c340d18317c2d25146ee2521e61b82f170
Paul Hutchin <paul.hutchin@flextrade.com>
2015-01-20 07:53:55 +0000

    Block deployment from travis

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 if [[ $TRAVIS_TAG =~ ^v.* ]]
 then
-    mvn deploy --settings ./settings.xml -DperformRelease=true
-
+  #  mvn deploy --settings ./settings.xml -DperformRelease=true
+    echo Not deploying from travis
 elif [[ $TRAVIS_BRANCH =~ stable/.* ]] || [[ $TRAVIS_BRANCH == 'master' ]]
 then
     mvn deploy --settings ./settings.xml
